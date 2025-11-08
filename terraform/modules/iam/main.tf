e81f5daa69019c69cb4578f85b2334215daeb994
Jenom <119628596+shehuj@users.noreply.github.com>
2025-11-08 13:49:11 -0600

    Update main.tf

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -57,3 +57,33 @@ resource "aws_iam_role_policy_attachment" "attach" {
   role       = aws_iam_role.ci_cd_role.name
   policy_arn = aws_iam_policy.ci_cd_policy.arn
 }
+
+resource "aws_lambda_function" "resume_pipeline" {
+  filename         = "lambda_function_payload.zip"
+  function_name    = "resume-deployment-pipeline"
+  role             = aws_iam_role.ci_cd_role.arn
+  handler          = "index.handler"
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+  runtime          = "nodejs14.x"
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.resume_pipeline.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.schedule_rule.arn
+}
+resource "aws_cloudwatch_event_rule" "schedule_rule" {
+  name                = "resume-deployment-schedule"
+  description         = "Triggers the resume deployment pipeline every day at midnight UTC"
+  schedule_expression = "rate(1 day)"
+}
+
+resource "aws_cloudwatch_event_target" "resume_pipeline_target" {
+  rule         = aws_cloudwatch_event_rule.schedule_rule.name
+  target_id  = "resume-pipeline-target"
+  arn          = aws_lambda_function.resume_pipeline.arn
+}
+
+  

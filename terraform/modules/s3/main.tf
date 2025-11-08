b418806d07b9b390b45b49cbb9e34a43878141a7
Jenom <119628596+shehuj@users.noreply.github.com>
2025-11-08 15:00:09 -0600

    Update main.tf

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "resume_site" {
   bucket = var.bucket_name
-  # Removed deprecated acl argument. Use bucket policies for access control.
   versioning {
     enabled = true
   }
@@ -20,10 +19,24 @@ resource "aws_s3_bucket" "resume_site" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "my_bucket_ownership" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+    # Other options: "BucketOwnerPreferred", "ObjectWriter" depending on your needs
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "block" {
   bucket                  = aws_s3_bucket.resume_site.id
-  block_public_acls       = true
-  ignore_public_acls      = true
-  block_public_policy     = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.my_bucket.id
+  policy = file("${path.module}/buckePolicy.json")
 }

555c0b6e3b7a727d07db9516707dc2c960e05fe6
Jenom <119628596+shehuj@users.noreply.github.com>
2025-11-08 09:12:08 -0600

    Update variables.tf

--- terraform/modules/dynamodb/variables.tf
+++ terraform/modules/dynamodb/variables.tf
@@ -1,11 +1,11 @@
 variable "table_deployment_tracking" {
   description = "value for deployment tracking table"
   type = string
-  default = "ec2-shutdown-deployment-tracking"
+  default = "table-deployment-tracking"
 }
 
 variable "table_resume_analytics" {
   description = "value for resume analytics table"
   type = string
-  default = "ec2-shutdown-resume-analytics"
+  default = "talbe-resume-analytics"
 }
